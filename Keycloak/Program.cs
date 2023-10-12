116a46e885e636c4435c8fe0515ec7dc0d88b4ca
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2023-10-12 18:57:43 +0300

    added keycloak authentication

--- Keycloak/Program.cs
+++ Keycloak/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
 namespace Keycloak
 {
   public class Program
@@ -12,7 +15,78 @@ namespace Keycloak
       builder.Services.AddControllers();
       // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
       builder.Services.AddEndpointsApiExplorer();
-      builder.Services.AddSwaggerGen();
+
+      builder.Services.AddSwaggerGen(c =>
+      {
+        // KeyCloak
+        c.CustomSchemaIds(type => type.ToString());
+
+        var securityScheme = new OpenApiSecurityScheme
+        {
+          Name = "KEYCLOAK",
+          Type = SecuritySchemeType.OAuth2,
+          In = ParameterLocation.Header,
+          BearerFormat = "JWT",
+          Scheme = "bearer",
+          Flows = new OpenApiOAuthFlows
+          {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+              AuthorizationUrl = new Uri(builder.Configuration["Jwt:AuthorizationUrl"]),
+              TokenUrl = new Uri(builder.Configuration["Jwt:TokenUrl"]),
+              Scopes = new Dictionary<string, string> { }
+            }
+          },
+          Reference = new OpenApiReference
+          {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+          }
+        };
+
+        c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement{
+                                                {securityScheme, new string[] { }}
+                                            });
+      });
+
+      // KeyCloak
+      builder.Services.AddAuthentication(options =>
+      {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+      }).AddJwtBearer(o =>
+      {
+        o.Authority = builder.Configuration["Jwt:Authority"];
+        o.Audience = builder.Configuration["Jwt:Audience"];
+        o.RequireHttpsMetadata = false;
+        o.Events = new JwtBearerEvents()
+        {
+          OnAuthenticationFailed = c =>
+          {
+            c.NoResult();
+
+            c.Response.StatusCode = 500;
+            c.Response.ContentType = "text/plain";
+
+            // Debug only for security reasons
+            // return c.Response.WriteAsync(c.Exception.ToString());
+
+            return c.Response.WriteAsync("An error occured processing your authentication.");
+          }
+        };
+      });
+
+      // Cross-Origin 
+      builder.Services
+          .AddCors(options =>
+          {
+            options.AddPolicy("AllowOrigin",
+          builder => builder.WithOrigins("http://localhost:4200")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod());
+          });
 
       var app = builder.Build();
 
@@ -20,11 +94,17 @@ namespace Keycloak
       if (app.Environment.IsDevelopment())
       {
         app.UseSwagger();
-        app.UseSwaggerUI();
+        app.UseSwaggerUI(c =>
+        {
+          c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyAppAPI");
+          c.OAuthClientId(builder.Configuration["Jwt:ClientId"]);
+          c.OAuthClientSecret(builder.Configuration["Jwt:ClientSecret"]);
+          c.OAuthRealm(builder.Configuration["Jwt:Realm"]);
+          c.OAuthAppName("KEYCLOAK");
+        });
       }
 
-      app.UseHttpsRedirection();
-
+      app.UseAuthentication();
       app.UseAuthorization();
 
 
