c821a9579186a4fc32de354de2f283c8d423dfd7
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2023-10-12 19:25:50 +0300

    updated settings

--- Keycloak/Controllers/WeatherForecastController.cs
+++ Keycloak/Controllers/WeatherForecastController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Keycloak.Controllers
 {
   [ApiController]
+  [Authorize]
   [Route("[controller]")]
   public class WeatherForecastController : ControllerBase
   {
